1ebfd3da9f4853636f19926441dabda859399a70
naflanmohamed <naflanmohammed611@gmail.com>
2024-02-22 00:35:52 +0500

    File Added For_Loop.js

---
+++ JS_Code/For_Loop.js
@@ -0,0 +1,26 @@
+//For Loop Syntax
+// for (statement 1; statement 2; statement 3) {
+//   // code block to be executed
+// }
+
+for(let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log("-------------------");
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for(let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+console.log("-------------------");
+
+let a = [];
+
+for(let i = 0; i <= 25; i++) {
+    a.push(i);
+}
+
+console.log(a);

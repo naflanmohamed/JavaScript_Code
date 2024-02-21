5ca30929724873fe9df3baca9772256b9831342d
naflanmohamed <naflanmohammed611@gmail.com>
2024-02-22 00:02:20 +0500

    If_Statement.js File Added

---
+++ JS_Code/If_Statement.js
@@ -0,0 +1,22 @@
+//If Statment
+
+var x = 10;
+if (x > 5) {
+    console.log("x is greater than 5");
+}
+else {
+    console.log("x is less than 5");
+}
+
+//If else if else
+
+let y = 4;
+if (y > 5) {
+    console.log("y is greater than 5");
+}
+else if (y < 5) {
+    console.log("y is less than 5");
+}
+else {
+    console.log("y is equal to 5");
+}

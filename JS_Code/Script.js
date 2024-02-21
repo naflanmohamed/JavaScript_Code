25fd28c1ecf9fafb9e22c8e8ef4209ab683fd0c6
naflanmohamed <naflanmohammed611@gmail.com>
2024-02-22 00:10:13 +0500

    Updated Script.js

--- JS_Code/Script.js
+++ JS_Code/Script.js
@@ -1,2 +1,19 @@
+let letter = prompt("Enter a letter: ");
 
-console.log("A" + "B" + 2);
+switch (letter) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        console.log("Vowel");
+        break;
+    default:
+        console.log("Consonant");
+        break;
+}
